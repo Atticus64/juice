a8c32dd9948a82a8b66ce775858b233824f9f1d5
Jona <jonathanelian64@gmail.com>
2022-12-30 09:22:03 -0700

    Format code in tests

--- tests/config_test.ts
+++ tests/config_test.ts
@@ -1,78 +1,69 @@
 import { assertEquals, assertThrows } from "testing";
-import { checkConfig, getWtFiles, changeConfig } from "$/config.ts";
-import { getFlags, Flags } from '$/flag.ts';
+import { changeConfig, checkConfig, getWtFiles } from "$/config.ts";
+import { Flags, getFlags } from "$/flag.ts";
 
 Deno.test("The checkConfig return true if path exists and return configFile path", async () => {
-  const result = await checkConfig(true)
+  const result = await checkConfig(true);
   assertEquals(result, ["./juice.json", true]);
 });
 
+Deno.test("getWtFiles function should return the content and path of settings file", async () => {
+  const [path, _] = await checkConfig(true);
+  const [content, configPath] = await getWtFiles(path);
 
-Deno.test('getWtFiles function should return the content and path of settings file', async () => {
+  const jsonContent = await Deno.readTextFile("./example-settings.json");
 
-  const [path, _] = await checkConfig(true)
-  const [content, configPath] = await getWtFiles(path)
+  assertEquals(content, jsonContent);
+  assertEquals(configPath, "./example-settings.json");
+});
 
-  const jsonContent = await Deno.readTextFile('./example-settings.json')
+Deno.test("changeConfig should change the options in config file", async () => {
+  const [path, _] = await checkConfig(true);
+  const [content, configPath] = await getWtFiles(path);
+  const flags: Flags = getFlags();
+  flags.font = "Minecraft Font";
+  flags.image = "creeper.jpg";
+  flags.scheme = "Dracula";
+  flags.terminal = "Hacker";
 
+  const jsonContent = JSON.parse(content);
 
-  assertEquals(content, jsonContent)
-  assertEquals(configPath, './example-settings.json')
+  const [data, _configString] = changeConfig(flags, jsonContent, configPath);
 
-})
+  const profiles = data.profiles.list;
 
-Deno.test('changeConfig should change the options in config file', async () => {
+  const hackerProfile = profiles.find((p) => p.name === "Hacker");
 
-  const [path, _] = await checkConfig(true)
-  const [content, configPath] = await getWtFiles(path)
-  const flags: Flags = getFlags()
-  flags.font = 'Minecraft Font'
-  flags.image = 'creeper.jpg'
-  flags.scheme = 'Dracula'
-  flags.terminal = 'Hacker'
+  assertEquals(hackerProfile?.font?.face, "Minecraft Font");
+  assertEquals(hackerProfile?.colorScheme, "Dracula");
+  assertEquals(hackerProfile?.backgroundImage, "creeper.jpg");
+});
 
-  const jsonContent = JSON.parse(content)
+Deno.test("changeConfig should modify in config file", async () => {
+  const [path, _] = await checkConfig(true);
+  const [content, configPath] = await getWtFiles(path);
+  const flags: Flags = getFlags();
+  flags.font = "Minecraft Font";
+  flags.image = "creeper.jpg";
+  flags.scheme = "Dracula";
+  flags.terminal = "Hacker";
 
-  const [data, _configString] = changeConfig(flags, jsonContent, configPath)
+  const jsonContent = JSON.parse(content);
 
-  const profiles = data.profiles.list
+  const jsonString = JSON.stringify(jsonContent, null, "\t");
 
-  const hackerProfile = profiles.find(p => p.name === 'Hacker');
+  const [_data, configString] = changeConfig(flags, jsonContent, configPath);
 
+  assertEquals(configString, jsonString);
+});
 
-  assertEquals(hackerProfile?.font?.face, 'Minecraft Font')
-  assertEquals(hackerProfile?.colorScheme, 'Dracula')
-  assertEquals(hackerProfile?.backgroundImage, 'creeper.jpg')
-
-})
-
-Deno.test('changeConfig should modify in config file', async () => {
-
-  const [path, _] = await checkConfig(true)
-  const [content, configPath] = await getWtFiles(path)
-  const flags: Flags = getFlags()
-  flags.font = 'Minecraft Font'
-  flags.image = 'creeper.jpg'
-  flags.scheme = 'Dracula'
-  flags.terminal = 'Hacker'
-
-  const jsonContent = JSON.parse(content)
-
-  const jsonString = JSON.stringify(jsonContent, null, "\t")
-
-  const [_data, configString] = changeConfig(flags, jsonContent, configPath)
-
-  assertEquals(configString, jsonString)
-})
-
-Deno.test('if dont especify the terminal should return error', async () => {
-
-  const [path, _] = await checkConfig(true)
-  const [content, configPath] = await getWtFiles(path)
-  const flags: Flags = getFlags()
-  flags.font = 'Minecraft Font'
-  flags.image = 'creeper.jpg'
-  flags.scheme = 'Dracula'
+Deno.test("if dont especify the terminal should return error", async () => {
+  const [path, _] = await checkConfig(true);
+  const [content, configPath] = await getWtFiles(path);
+  const flags: Flags = getFlags();
+  flags.font = "Minecraft Font";
+  flags.image = "creeper.jpg";
+  flags.scheme = "Dracula";
 
   const jsonContent = JSON.parse(content);
 
@@ -81,5 +72,4 @@ Deno.test('if dont especify the terminal should return error', async () => {
     Error,
     "You need to specify the terminal to apply the config",
   );
-
-})
+});
